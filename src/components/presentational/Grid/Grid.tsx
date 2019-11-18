6f3c0ceb78679706c1251cf84bf16635fb1c2c0b
Sebastian Southern <sebastian.southern@gmail.com>
2019-11-18 18:32:28 +1100

    checking for isBlocked on click and in isAdjacent

--- src/components/presentational/Grid/Grid.tsx
+++ src/components/presentational/Grid/Grid.tsx
@@ -15,6 +15,7 @@ const Grid: React.FunctionComponent<GridProps> = ({ size }) => {
   const [gridPath, setGridPath] = React.useState<any>({});
   const [grid, setGrid] = React.useState([]);
   const [blocked, setBlocked] = React.useState<any>({
+    "5,0": true,
     "5,1": true,
     "5,2": true,
     "5,3": true,
@@ -44,34 +45,17 @@ const Grid: React.FunctionComponent<GridProps> = ({ size }) => {
       left: [x-1, y]
     };
     const arr = [];
-    if (adjacentVals.top[1] >= 0) {
-      // TODO: Need to push, something that reflects its a wall
-      if (adjacentVals.top[0] === 5 && adjacentVals.top[1] !== 3) {
-        // console.log('Trying to go up ' + x + ' y is ' + y);
-      } else {
-        arr.push(adjacentVals.top);
-      }
+    if (!isBlocked(adjacentVals.top) && adjacentVals.top[1] >= 0) {
+      arr.push(adjacentVals.top);
     }
-    if (adjacentVals.right[0] <= size - 1) {
-      if (adjacentVals.right[0] === 5 && adjacentVals.right[1] !== 3) {
-        // console.log('trying to go right ' + x + ' y is ' + y);
-      } else {
-        arr.push(adjacentVals.right);
-      }
+    if (!isBlocked(adjacentVals.right) && adjacentVals.right[0] <= size - 1) {
+      arr.push(adjacentVals.right);
     }
-    if (adjacentVals.bottom[1] <= size - 1) {
-      if (adjacentVals.bottom[0] === 5 && adjacentVals.bottom[1] !== 3) {
-        // console.log('trying to go down ' + x + ' y is ' + y);
-      } else {
-        arr.push(adjacentVals.bottom);
-      }
+    if (!isBlocked(adjacentVals.bottom) && adjacentVals.bottom[1] <= size - 1) {
+      arr.push(adjacentVals.bottom);
     }
-    if (adjacentVals.left[0] >= 0) {
-      if (adjacentVals.left[0] === 5 && adjacentVals.left[1] !== 3) {
-        // console.log('trying to go left ' + x + ' and y is ' + y);
-      } else {
+    if (!isBlocked(adjacentVals.left) && adjacentVals.left[0] >= 0) {
         arr.push(adjacentVals.left);
-      }
     }
     // console.log('arr is ', arr);
     return arr;
@@ -87,7 +71,6 @@ const Grid: React.FunctionComponent<GridProps> = ({ size }) => {
     while (unprocessed.length !== 0) {
       let u: any = unprocessed.shift();
       if (u[0] === selectedPosition[0] && u[1] === selectedPosition[1]) {
-        console.log('selected:', selectedPosition, ' was found at position', u);
         break;
       }
       // console.log('u is ', u, 'selected is ', selectedPosition);
@@ -114,7 +97,6 @@ const Grid: React.FunctionComponent<GridProps> = ({ size }) => {
     } else if (arr[0] === selectedPosition[0] && arr[1] === selectedPosition[1]) {
       return columnSelectedStyle;
     } else if (result[arr]) {
-      console.log('gridpath was selected', arr, gridPath[arr], gridPath);
       return columnPathStyle;
     } else {
       return columnStyle;
@@ -126,7 +108,6 @@ const Grid: React.FunctionComponent<GridProps> = ({ size }) => {
     if (selectedPosition.length > 0) {
       result = breadthFirstTraversal([0,0]);
       setGridPath(result);
-      console.log('result is ', result);
     }
     const createMatrix = () => {
       let tmpGrid: any = [];
@@ -154,10 +135,10 @@ const Grid: React.FunctionComponent<GridProps> = ({ size }) => {
 
 
   const handleSetSelectedPosition = (position: number[]) => {
-    setSelectedPosition(position);
+    if (!isBlocked(position)) {
+      setSelectedPosition(position);
+    }
   };
-  
-  // console.log('testArr', isBlocked(testArr));
 
   return (
     <div className={styles.grid}>
